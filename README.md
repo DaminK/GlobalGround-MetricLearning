f5681c455badeda15a31cbca23c7bbc20c137ddc
damin <damin.kuehn@rwth-aachen.de>
2025-03-24 14:47:20 +0000

    env file for convenience

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 Global Ground Metric Learning 
 ====================
-This repository provides the python implementation of the AISTATS2025 paper "Global Ground Metric Learning with Applications to scRNA data" by Damin Kühn nad Michael T. Schaub.
+This repository provides the python implementation of the AISTATS2025 paper "Global Ground Metric Learning with Applications to scRNA data" by Damin Kühn and Michael T. Schaub.
 
 ## Abstract
 Optimal transport (OT) provides a robust framework for comparing probability distributions. Its effectiveness is significantly influenced by the choice of the underlying ground metric. Traditionally, the ground metric has either been (i) predefined, e.g. as a Euclidean metric, or (ii) learned in a supervised way, by utilizing labeled data to learn a suitable ground metric for enhanced task-specific performance. While predefined metrics often do not account for the inherent structure and varying significance of different features in the data, existing supervised ground metric learning methods often fail to generalize across multiple classes or are limited to distributions with shared supports. To address this issue, this paper introduces a novel approach for learning metrics for arbitrary distributions over a shared metric space. Our method differentiates elements like a global metric, but requires only class labels on a distribution-level for training akin a ground metric. The resulting learned global ground metric enables more accurate OT distances, which can significantly improve clustering and classification tasks. It can create task-specific shared embeddings across elements of different distributions including unseen data.
 
 ## Repository Structure
-Tutorials on how to use GGML on synthetic or real-world scRNA data are provided as jupyter notebooks in (code).
+Tutorials on how to use GGML on synthetic or real-world scRNA data are provided as Jupyter notebooks in `code/`.
 
-The classification and clustering experiments are provided as jupyter notebooks in (code/reproduce_experiments).
+The classification and clustering experiments are provided in `code/reproduce_experiments/`. Use the python script to compute the results and the Jupyter notebooks to visualize the results.
 
 ## Setup
-Venv file for convienent use will follow
+To set-up the environment you can use the env file provided in `code/env/ggml.yaml`.
 
--- code/ggml/data/scRNA.py
+++ code/ggml/data/scRNA.py
@@ -29,7 +29,6 @@ from sklearn.decomposition import PCA
 import scipy as sp
 
 class scRNA_Dataset(Dataset):
-    #The __init__ function is run once when instantiating the Dataset object.
     def __init__(self, *args, **kwargs):
         #Generate syntehtic data
         distributions, distributions_labels, points, point_labels, disease_labels, celltype_node_labels, patient_labels = get_cells_by_patients(*args, **kwargs)
@@ -40,13 +39,13 @@ class scRNA_Dataset(Dataset):
         self.disease_labels = disease_labels
         self.patient_labels = patient_labels
 
-        #Unit-level  TODO
+        #Unit-level  
         self.datapoints = points 
         self.datapoints_labels = point_labels
         self.celltype_node_labels = celltype_node_labels 
 
         #Triplets
-        self.triplets = create_t_triplets(distributions,distributions_labels,**kwargs) #TODO neighbors as param
+        self.triplets = create_t_triplets(distributions,distributions_labels,**kwargs) 
 
     def __len__(self):
         #Datapoints to train are always given as triplets
@@ -79,248 +78,9 @@ class scRNA_Dataset(Dataset):
 
         hier_clustering(D,self.disease_labels, ax=None,dist_name="W_θ")
         return D
-
-
-def full_get_cells_by_patients(adata, patient_col="donor_id",label_col="reported_diseases",subsample_patient_ratio=1.0,n_feats=None,filter_genes=False,n_cells=None,**kwargs):
-    if isinstance(adata,str):
-        adata = sc.read_h5ad(adata)
     
-
-    string_class_labels = np.unique(adata.obs[label_col])
-
-    #detect low variable genes
-    if filter_genes:
-        gene_var = np.var(adata.X.toarray(),axis=0)
-
-        #filter
-        thresh = np.mean(gene_var) #TODO make this not hardcoded and arbitrary
-        adata = adata[:,gene_var >thresh]
-
-    #TODO don't let anyone see this 
-    distributions = []
-    distributions_class = []
-    patient_labels = []
-    disease_labels = []
-    celltype_node_label = []
-    dataset_labels = []
-    origin_node_label = []
-    patient_cell_labels = []
-
-
-
-    unique_patients = np.unique(adata.obs[patient_col])
-    unique_patients_subsampled = np.random.choice(unique_patients, size = int(len(unique_patients)*subsample_patient_ratio),replace=False)
-
-
-    for patient in unique_patients_subsampled:
-
-
-        patient_adata = adata[adata.obs[patient_col] == patient]
-
-        disease_label = np.unique(patient_adata.obs[label_col].to_numpy())
-        string_class_label = disease_label[0]
-        if len(disease_label) > 1:
-            print("Warning, sample_ids refer to cells with multiple disease labels (likely caused by referencing by patients and having multiple samples from different zones)")
-
-        '''
-        #This is only relevant for datasets that contain multiple samples
-        unique,pos = np.unique(patient_adata.obs[label_col].to_numpy(),return_inverse=True) #Finds all unique elements and their positions
-        for sample in unique:
-
-
-        counts = np.bincount(pos)                     #Count the number of each unique element
-        maxpos = counts.argmax()                      #Finds the positions of the maximum count
-        string_class_label = unique[maxpos] #we take the label that occurs most often for this patient
-        patient_adata = adata[adata.obs[label_col] == string_class_label ] # and only keep those (this is only relevant for myocardial infarction)
-        '''
-
-        if n_cells is None or patient_adata.n_obs >= n_cells:
-            
-            if n_cells is not None:
-                sc.pp.subsample(patient_adata,n_obs = n_cells) 
-
-            #if n_feats == 50:
-            #    p_arr = np.asarray(patient_adata.obsm["X_pca"],dtype="f")
-            #else:
-            p_arr = np.asarray(patient_adata.X.toarray(),dtype="f") #TODO directly sparse scipy matrix to numpy matrix?
-
-
-            distributions.append(p_arr)
-            
-            disease_labels.append(string_class_label)
-            distributions_class.append(np.where(string_class_labels==string_class_label)[0][0])
-            patient_labels.append(patient)
-            patient_cell_labels.append(list(patient_adata.obs['sample']))
-            celltype_node_label.append(list(patient_adata.obs['cell_subtype']))
-            origin_node_label.append(list(patient_adata.obs['dataset']))
-
-            dataset_labels.append(patient_adata.obs["dataset"][0])
-        else:
-            print(f"patient {patient} has {patient_adata.n_obs} cells")
-
-    points = np.concatenate(distributions) #np.reshape(np.asarray(dists),(-1,2))
-    point_labels = sum([[l] * len(D) for l,D in zip(disease_labels,distributions)],[])
-    
-
-    return distributions, distributions_class, points, point_labels,  patient_labels, disease_labels, celltype_node_label, dataset_labels, origin_node_label, patient_cell_labels
-
-def get_cells_by_patients(adata_path,patient_col="donor_id",label_col="reported_diseases",subsample_patient_ratio=1.0,n_feats=None,max_cells = None,filter_genes=False,**kwargs):
-    global adata
-    adata = sc.read_h5ad(adata_path+".h5ad")  #adata_path+".h5ad")
-    print(adata)
-
-    string_class_labels = np.unique(adata.obs[label_col])
-
-    if filter_genes:
-        #detect low variable genes
-        gene_var = np.var(adata.X.toarray(),axis=0)
-        #filter
-        thresh = np.mean(gene_var) #TODO make this not hardcoded and arbitrary
-        adata = adata[:,gene_var >thresh]
-        #adata.write(adata_path+"_filtered.h5ad")
-
-    distributions = []
-    distributions_class = []
-    patient_labels = []
-    disease_labels = []
-    celltype_node_label = []
-
-    if n_feats is not None:
-        global pca
-        pca = PCA(n_components=n_feats, svd_solver='auto')
-        pca.fit(adata.X)
-    unique_patients = np.unique(adata.obs[patient_col])
-    unique_patients_subsampled = np.random.choice(unique_patients, size = int(len(unique_patients)*subsample_patient_ratio),replace=False)
-
-
-    for patient in unique_patients_subsampled:
-        
-        
-        patient_adata = adata[adata.obs[patient_col] == patient]
-
-        disease_label = np.unique(patient_adata.obs[label_col].to_numpy())
-        string_class_label = disease_label[0]
-        
-        if len(disease_label) > 1:
-            print("Warning, sample_ids refer to cells with multiple disease labels (likely caused by referencing by patients and having multiple samples from different zones)")
-
-        '''
-        unique,pos = np.unique(patient_adata.obs[label_col].to_numpy(),return_inverse=True) #Finds all unique elements and their positions
-        counts = np.bincount(pos)                     #Count the number of each unique element
-        maxpos = counts.argmax()                      #Finds the positions of the maximum count
-        string_class_label = unique[maxpos] #we take the label that occurs most often for this patient
-        patient_adata = adata[adata.obs[label_col] == string_class_label ] # and only keep those (this is only relevant for myocardial infarction)
-        '''
-
-        if max_cells is not None and patient_adata.n_obs > max_cells:
-            sc.pp.subsample(patient_adata,n_obs = max_cells) 
-
-        #p_arr = np.asarray(patient_adata.X.toarray(),dtype="f") #TODO directly sparse scipy matrix to numpy matrix?
-
-        #if n_feats == 50:
-        #    p_arr = np.asarray(patient_adata.obsm["X_pca"],dtype="f")
-        #else:
-        p_arr = np.asarray(patient_adata.X.toarray(),dtype="f") #TODO directly sparse scipy matrix to numpy matrix?
-        if n_feats is not None:
-            p_arr = pca.transform(p_arr)
-
-        distributions.append(p_arr)
-        
-        disease_labels.append(string_class_label)
-        #distributions_class.append(np.where(string_class_labels==string_class_label)[0][0])
-        patient_labels.append(list(patient_adata.obs[patient_col]))
-        celltype_node_label.append(list(patient_adata.obs['cell_type'])) #cell_Type
-            
-        #Cell level
-    points = np.concatenate(distributions) #np.reshape(np.asarray(dists),(-1,2))
-    point_labels = sum([[l] * len(D) for l,D in zip(disease_labels,distributions)],[]) #flattens list of lists
-
-    #return distributions, distributions_class, patient_labels, disease_labels, celltype_node_label
-    return distributions, disease_labels, points, point_labels, disease_labels, celltype_node_label, np.concatenate(patient_labels)
-
-
-#TODO check diffs between cells load funcs of patient
-def get_cells_by_patients_2(adata_path,patient_col="donor_id",label_col="reported_diseases",subsample_patient_ratio=1.0,n_cells=1000,pca_components=None,filter_genes=False,**kwargs):
-    adata = sc.read_h5ad(adata_path+".h5ad")
-
-    string_class_labels = np.unique(adata.obs[label_col])
-
-    #detect low variable genes
-    if filter_genes:
-        gene_var = np.var(adata.X.toarray(),axis=0)
-        org_genes = len(gene_var)
-
-        #filter
-        thresh = np.mean(gene_var) #TODO make this not hardcoded and arbitrary
-        adata = adata[:,gene_var >thresh]
-        filtered_genes = np.asarray([gene_var >thresh]).sum()
-        print(f"Removed {org_genes - filtered_genes} low variance genes out of {org_genes} genes")
-
-
-    distributions = []
-    distributions_class = []
-    patient_labels = []
-    disease_labels = []
-    celltype_node_label = []
-
-    if pca_components is not None:
-        pca = PCA(n_components=pca_components, svd_solver='auto')
-        pca.fit(adata.X)
-
-    unique_patients = np.unique(adata.obs[patient_col])
-    unique_patients_subsampled = np.random.choice(unique_patients, size = int(len(unique_patients)*subsample_patient_ratio),replace=False)
-
-
-    for patient in unique_patients_subsampled:
-
-
-        patient_adata = adata[adata.obs[patient_col] == patient]
-
-        disease_label = np.unique(patient_adata.obs[label_col].to_numpy())
-        string_class_label = disease_label[0]
-        if len(disease_label) > 1:
-            print("Warning, sample_ids refer to cells with multiple disease labels (likely caused by referencing by patients and having multiple samples from different zones)")
-
-        '''
-        #This is only relevant for datasets that contain multiple samples
-        unique,pos = np.unique(patient_adata.obs[label_col].to_numpy(),return_inverse=True) #Finds all unique elements and their positions
-        for sample in unique:
-
-
-        counts = np.bincount(pos)                     #Count the number of each unique element
-        maxpos = counts.argmax()                      #Finds the positions of the maximum count
-        string_class_label = unique[maxpos] #we take the label that occurs most often for this patient
-        patient_adata = adata[adata.obs[label_col] == string_class_label ] # and only keep those (this is only relevant for myocardial infarction)
-        '''
-
-        if patient_adata.n_obs >= n_cells:
-            
-
-            sc.pp.subsample(patient_adata,n_obs = n_cells) 
-
-            #if n_feats == 50:
-            #    p_arr = np.asarray(patient_adata.obsm["X_pca"],dtype="f")
-            #else:
-            p_arr = np.asarray(patient_adata.X.toarray(),dtype="f") #TODO directly sparse scipy matrix to numpy matrix?
-            if pca_components is not None:
-                p_arr = pca.transform(p_arr)
-
-            distributions.append(p_arr)
-            
-            disease_labels.append(string_class_label)
-            distributions_class.append(np.where(string_class_labels==string_class_label)[0][0])
-            patient_labels.append(patient)
-            celltype_node_label.append(list(patient_adata.obs['cell_type']))
-            
-   
-
-    return distributions, distributions_class, patient_labels, disease_labels, celltype_node_label
-
-
-def get_cells_by_patients_3(adata_path,patient_col="donor_id",label_col="reported_diseases",subsample_patient_ratio=0.25,n_feats=None,filter_genes=False,**kwargs):
-    global adata
+def get_cells_by_patients(adata_path,patient_col="donor_id",label_col="reported_diseases",subsample_patient_ratio=0.25,n_cells=1.0,n_feats=None,filter_genes=False,**kwargs):
     adata = sc.read_h5ad(adata_path)
-
     string_class_labels = np.unique(adata.obs[label_col])
 
     #detect low variable genes
@@ -328,9 +88,9 @@ def get_cells_by_patients_3(adata_path,patient_col="donor_id",label_col="reporte
         gene_var = np.var(adata.X.toarray(),axis=0)
 
         #filter
-        thresh = np.mean(gene_var) #TODO make this not hardcoded and arbitrary
+        thresh = np.mean(gene_var) 
         adata = adata[:,gene_var >thresh]
-
+        print(adata)
 
     distributions = []
     distributions_class = []
@@ -346,49 +106,27 @@ def get_cells_by_patients_3(adata_path,patient_col="donor_id",label_col="reporte
     unique_patients = np.unique(adata.obs[patient_col])
     unique_patients_subsampled = np.random.choice(unique_patients, size = int(len(unique_patients)*subsample_patient_ratio),replace=False)
 
-
     for patient in unique_patients_subsampled:
-
-
         patient_adata = adata[adata.obs[patient_col] == patient]
-
         disease_label = np.unique(patient_adata.obs[label_col].to_numpy())
         string_class_label = disease_label[0]
+
         if len(disease_label) > 1:
             print("Warning, sample_ids refer to cells with multiple disease labels (likely caused by referencing by patients and having multiple samples from different zones)")
 
-        '''
-        #This is only relevant for datasets that contain multiple samples
-        unique,pos = np.unique(patient_adata.obs[label_col].to_numpy(),return_inverse=True) #Finds all unique elements and their positions
-        for sample in unique:
-
-
-        counts = np.bincount(pos)                     #Count the number of each unique element
-        maxpos = counts.argmax()                      #Finds the positions of the maximum count
-        string_class_label = unique[maxpos] #we take the label that occurs most often for this patient
-        patient_adata = adata[adata.obs[label_col] == string_class_label ] # and only keep those (this is only relevant for myocardial infarction)
-        '''
-
         if patient_adata.n_obs >= n_cells:
-            
-
             sc.pp.subsample(patient_adata,n_obs = n_cells) 
-
-            #if n_feats == 50:
-            #    p_arr = np.asarray(patient_adata.obsm["X_pca"],dtype="f")
-            #else:
-            p_arr = np.asarray(patient_adata.X.toarray(),dtype="f") #TODO directly sparse scipy matrix to numpy matrix?
+            p_arr = np.asarray(patient_adata.X.toarray(),dtype="f") 
             if n_feats is not None:
                 p_arr = pca.transform(p_arr)
 
             distributions.append(p_arr)
-            
             disease_labels.append(string_class_label)
             distributions_class.append(np.where(string_class_labels==string_class_label)[0][0])
             patient_labels.append(patient)
             celltype_node_label.append(list(patient_adata.obs['cell_type']))
-            
 
-    
+    points = np.concatenate(distributions) 
+    point_labels = sum([[l] * len(D) for l,D in zip(disease_labels,distributions)],[]) 
 
-    return distributions, distributions_class, patient_labels, disease_labels, celltype_node_label
+    return distributions, distributions_class, points, point_labels, disease_labels, celltype_node_label, patient_labels
